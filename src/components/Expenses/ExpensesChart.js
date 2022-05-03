29f14d1aa68f70103ad62d15bbc812550cfa9621
Sandro Sena <sandro340@hotmail.com>
2022-05-03 20:08:23 -0300

    Commit 6.1: Fix Months

--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -2,20 +2,22 @@ import Chart from "../Chart/Chart";
 
 const ExpensesChart = (props) => {
   const chartDataPoints = [
-    { label: 'Janeiro', value: 0 },
-    { label: 'Fevereiro', value: 0 },
-    { label: 'Março', value: 0 },
-    { label: 'Abril', value: 0 },
-    { label: 'Maio', value: 0 },
-    { label: 'Junho', value: 0 },
-    { label: 'Julho', value: 0 },
-    { label: 'Agosto', value: 0 },
-    { label: 'Novembro', value: 0 },
-    { label: 'Dezembro', value: 0 }
+    { label: 'Jan', value: 0 },
+    { label: 'Feb', value: 0 },
+    { label: 'Mar', value: 0 },
+    { label: 'Apr', value: 0 },
+    { label: 'May', value: 0 },
+    { label: 'Jun', value: 0 },
+    { label: 'Jul', value: 0 },
+    { label: 'Aug', value: 0 },
+    { label: 'Sep', value: 0 },
+    { label: 'Oct', value: 0 },
+    { label: 'Nov', value: 0 },
+    { label: 'Dec', value: 0 },
   ];
 
   for (const expense of props.expenses) {
-    const expenseMonth = expense.date.getMonth(); // Janeiro = 0
+    const expenseMonth = expense.date.getMonth(); // January = 0
     chartDataPoints[expenseMonth].value += expense.amount;
   }
 

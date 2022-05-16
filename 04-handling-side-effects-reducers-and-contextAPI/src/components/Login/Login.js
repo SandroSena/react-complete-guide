f32704fe7dcf60b25d642abfc6020809b0ebcba4
SandroSena <sandro340@hotmail.com>
2022-05-16 10:20:00 -0300

    UseEffect

--- 04-handling-side-effects-reducers-and-contextAPI/src/components/Login/Login.js
+++ 04-handling-side-effects-reducers-and-contextAPI/src/components/Login/Login.js
@@ -12,10 +12,26 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    )
-  }, [enteredEmail, enteredPassword])
+    console.log('Effect Running');
+
+    return () => {
+      console.log('Effect CleanUp')
+    }
+  }, []);
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      console.log('Checking Form Validity')
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log('CleanUp');
+      clearTimeout(identifier);
+    };
+  }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);

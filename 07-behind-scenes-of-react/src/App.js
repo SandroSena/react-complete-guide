f9a95e40e7a9e6270d4d5229d969ca835345b7bf
SandroSena <sandro340@hotmail.com>
2022-05-26 11:42:21 -0300

    UseMemo, UseCallBack and React.Memo

--- 07-behind-scenes-of-react/src/App.js
+++ 07-behind-scenes-of-react/src/App.js
@@ -1,21 +1,22 @@
-import React, { useState } from 'react';
-import Button from './components/UI/Button/Button';
-import DemoOutput from './components/UI/Button/Demo/DemoOutput';
+import React, { useState, useCallback, useMemo } from 'react';
 
 import './App.css';
+import DemoList from './components/UI/Demo/DemoList';
+import Button from './components/UI/Button/Button';
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState(false);
+  const [listTitle, setListTitle] = useState('My List');
 
-  console.log('APP Running!')
+  const changeTitleHandler = useCallback(() => {
+    setListTitle('New Title');
+  }, []);
 
-  const toggleParagraph = () => setShowParagraph(() => !showParagraph);
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
 
   return (
     <div className="app">
-      <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph} />
-      <Button onClick={toggleParagraph}>Toggle Paragraph</Button>
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
     </div>
   );
 }

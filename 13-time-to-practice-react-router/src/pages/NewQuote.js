7388f65139cc523eabe88b111651e6f214733ef9
SandroSena <sandro340@hotmail.com>
2022-07-21 10:41:54 -0300

    Programmatic (Imperative) Navigation with React Router

--- 13-time-to-practice-react-router/src/pages/NewQuote.js
+++ 13-time-to-practice-react-router/src/pages/NewQuote.js
@@ -1,8 +1,14 @@
+import { useHistory } from "react-router-dom";
+
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const AddQuoteHandler = quoteData => {
-    console.log(quoteData)
+    console.log(quoteData);
+
+    history.push('/quotes');
   }
 
 

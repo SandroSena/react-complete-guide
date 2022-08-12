238e9404bfe2a4f5c790b7ad76896df35010843c
SandroSena <sandro340@hotmail.com>
2022-08-12 10:22:47 -0300

    Showing Feedback To The User

--- 14-authentication-on-react-app/src/components/Auth/AuthForm.js
+++ 14-authentication-on-react-app/src/components/Auth/AuthForm.js
@@ -7,6 +7,7 @@ const AuthForm = () => {
   const passwordInputRef = useRef();
 
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -20,6 +21,8 @@ const AuthForm = () => {
 
     // optional: Add validation
 
+
+    setIsLoading(true);
     if (isLogin) {
     } else {
       /** Using Async Await
@@ -47,12 +50,16 @@ const AuthForm = () => {
           },
         }
       ).then((res) => {
+        setIsLoading(false);
         if (res.ok) {
           // ...
         } else {
           return res.json().then((data) => {
-            // show an error modal
-            console.log(data);
+            let errorMessage = 'Authentication Failed!';
+            // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
+            alert(errorMessage);
           });
         }
       });
@@ -77,7 +84,8 @@ const AuthForm = () => {
           />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {isLoading && <p>Sending Request...</p>}
           <button
             type='button'
             className={classes.toggle}

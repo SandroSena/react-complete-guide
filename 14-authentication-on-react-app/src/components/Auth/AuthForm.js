d455745687fc213f5ef0af8166a2f3062742048b
SandroSena <sandro340@hotmail.com>
2022-08-16 10:05:16 -0300

    Adding User Login

--- 14-authentication-on-react-app/src/components/Auth/AuthForm.js
+++ 14-authentication-on-react-app/src/components/Auth/AuthForm.js
@@ -23,47 +23,43 @@ const AuthForm = () => {
 
 
     setIsLoading(true);
+    let url;
     if (isLogin) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB9mpMcOAyR3WduJzaorcQKsy_casrvs_Q'
     } else {
-      /** Using Async Await
-            await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB9mpMcOAyR3WduJzaorcQKsy_casrvs_Q',
-              {
-                method: 'POST',
-                body: JSON.stringify({
-                  email: enteredEmail,
-                  password: enteredPassword,
-                  returnSecureToken: true,
-                }),
-              });
-       */
-      fetch(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB9mpMcOAyR3WduJzaorcQKsy_casrvs_Q',
-        {
-          method: 'POST',
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      ).then((res) => {
-        setIsLoading(false);
-        if (res.ok) {
-          // ...
-        } else {
-          return res.json().then((data) => {
-            let errorMessage = 'Authentication Failed!';
-            // if (data && data.error && data.error.message) {
-            //   errorMessage = data.error.message;
-            // }
-            alert(errorMessage);
-          });
-        }
-      });
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB9mpMcOAyR3WduJzaorcQKsy_casrvs_Q'
     }
+    fetch(
+      url,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    ).then((res) => {
+      setIsLoading(false);
+      if (res.ok) {
+        return res.json();
+      } else {
+        return res.json().then((data) => {
+          let errorMessage = 'Authentication Failed!';
+          // if (data && data.error && data.error.message) {
+          //   errorMessage = data.error.message;
+          // }
+          throw new Error(errorMessage);
+        });
+      }
+    }).then(data => { 
+      console.log(data);
+    }).catch(err => {
+      alert(err.message);
+    })
   };
 
   return (
